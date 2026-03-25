f5a2a8781434a0c2433d815402f4ac0da9e25def
Arefcy Saban <andriyanto.ay595@gmail.com>
2026-03-25 20:08:11 +0800

    Remove comments and clean up DetailActivity.kt

--- app/src/main/java/com/example/aplikasiregistrasi/DetailActivity.kt
+++ app/src/main/java/com/example/aplikasiregistrasi/DetailActivity.kt
@@ -7,14 +7,13 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
         val tvHasil = findViewById<TextView>(R.id.tvHasil)
-// 1. Menangkap data yang dikirim dari MainActivity
-// Harus menggunakan "Kunci" yang sama persis saat mengirim
+
         val namaDiterima = intent.getStringExtra("EXTRA_NAMA")
         val nimDiterima = intent.getStringExtra("EXTRA_NIM")
         val prodiDiterima = intent.getStringExtra("EXTRA_PRODI")
-// 2. Menyusun teks hasil (menggunakan \n untuk baris baru)
+
         val teksHasil = "Nama: $namaDiterima \nNIM: $nimDiterima \nProgramStudi: $prodiDiterima"
-// 3. Menampilkan ke layar
+
         tvHasil.text = teksHasil
     }
 }
